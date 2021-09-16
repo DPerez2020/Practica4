531c14669204dd3db91c6f8142f5f3953bc16570
Dawin Rafael Perez Batista <dperez@pixnabilab.com>
2021-09-16 10:05:34 -0400

    Agregar archivos de proyecto.

---
+++ ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica4
+{
+    /*1 - Hacer un método extendido que opere sobre el tipo List<int> llamado Pares() que retorne un List<int> con los valores pares en la lista asociada dada. 
+    En la implementación del método Pares() utilice el método  FindAll() de las listas para pasarle una función 
+    predicado mediante la implementación de un delegado que represente métodos que retornen bool y que reciban un entero como parámetro.*/
+    delegate bool delegado(int x);
+
+    public static class ejercicio1
+    {       
+        public static List<int> Pares(this List<int> lista)
+        {
+            delegado pares = Par;
+            return lista.FindAll(Par);
+        }
+        private static bool Par(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+
+}
---
+++ ejercicio2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4
+{
+    /*
+     2 -  Escriba los siguiente método como expresión lambda:
+        public bool EsImpar(int x){
+             if (x % 2 != 0){
+                 return true;
+             else{
+                 return false;
+             }
+        }
+
+
+        public double CalcularITBIS(double precio){
+            var itebis = precio * 0.18;
+            return itebis;
+        }
+
+
+        public void GetDate(){
+             var fecha = DateTime.Now;
+             return fecha;
+        }
+     */
+    public delegate double calcularItebis(double precio);
+    public delegate DateTime getDate();
+    public delegate bool esImpar(int x);
+
+    class ejercicio2
+    {
+        calcularItebis calcularItebis = (x) => x * 0.18;
+        getDate getDate = () => DateTime.Now;
+        esImpar esImpar = (x) => x % 2 != 0;
+    }
+}
